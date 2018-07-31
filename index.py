d7366f19cdd13fa4e82711cccefb640ea5ec1895
Cardiac Robot <cardiac.robot@gmail.com>
2018-07-31 11:48:36 -0500

    relaunch bug in windows fixed

--- index.py
+++ index.py
@@ -8,41 +8,44 @@ import utilities.project as PJ
 import plugins.MainPlugin as MP
 #import data handler and database
 import db.database as database
-#OS system verification
-s = sys.platform
 
-#windows 32 bits case
-if s == 'win32':
-    #import ctypes to get screen resolution
-    import ctypes
-    #get resolution
-    user32=ctypes.windll.user32
-    resolution = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-    res = {'width': int(resolution[0]), 'height': int(resolution[1])}
 
-#linux case
-elif s == 'linux2':
-    #import subprocess to get screen resolution
-    import subprocess
-    #get resolution
-    output = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
-    resolution = output.split()[0].split(b'x')
-    res = {'width': int(resolution[0]), 'height': int(resolution[1])}
-    print type(res['width'])
+if __name__ == '__main__':
+    #OS system verification
+    s = sys.platform
 
-#other platform not supported
-else:
-    print 'No supported system'
-    #finish the program safe
-    sys.exit(0)
+    #windows 32 bits case
+    if s == 'win32':
+        #import ctypes to get screen resolution
+        import ctypes
+        #get resolution
+        user32=ctypes.windll.user32
+        resolution = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
+        res = {'width': int(resolution[0]), 'height': int(resolution[1])}
 
-#create settings sdictionary to pass it to de the project handler
-settings = {'res': res, 'sys': s}
-#project handler load, paths
-ph = PJ.ProjectHandler(settings = settings , log = True)
-#database creation
-db = database.database(ProjectHandler = ph)
-#launch entry point of the interface
-app = QtGui.QApplication(sys.argv)
-main = MP.MainPlugin(ProjectHandler = ph, DataHandler = db)
-sys.exit(app.exec_())
+    #linux case
+    elif s == 'linux2':
+        #import subprocess to get screen resolution
+        import subprocess
+        #get resolution
+        output = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
+        resolution = output.split()[0].split(b'x')
+        res = {'width': int(resolution[0]), 'height': int(resolution[1])}
+        print type(res['width'])
+
+    #other platform not supported
+    else:
+        print 'No supported system'
+        #finish the program safe
+        sys.exit(0)
+
+    #create settings sdictionary to pass it to de the project handler
+    settings = {'res': res, 'sys': s}
+    #project handler load, paths
+    ph = PJ.ProjectHandler(settings = settings , log = True)
+    #database creation
+    db = database.database(ProjectHandler = ph)
+    #launch entry point of the interface
+    app = QtGui.QApplication(sys.argv)
+    main = MP.MainPlugin(ProjectHandler = ph, DataHandler = db)
+    sys.exit(app.exec_())
